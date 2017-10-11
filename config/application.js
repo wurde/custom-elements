492168d4e66cef808016ff4a53ab23d727f2118f
Andy Bettisworth <andy@accreu.com>
2017-10-11 11:37:50 -0500

    get baseline electron app to render

---
+++ config/application.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const express = require('express')
+const app = express()
+const path = require('path')
+
+let port = 53011
+let root = path.join(__dirname, '/..')
+
+//
+// Application variables
+//
+app.locals.title = "ThemeBuilder"
+app.locals.root  = root
+app.locals.views = root + '/app/views'
+
+//
+// Settings
+//
+app.disable('x-powered-by')
+
+//
+// View engine: Embedded JavaScript - ejs.co
+//
+app.engine('html.ejs', require('ejs').renderFile);
+app.set('views', app.locals.views)
+app.set('view engine', 'html.ejs')
+
+//
+// Static assets
+//
+app.use(express.static(root + '/public'))
+app.use('/assets', express.static(root + '/node_modules/jquery/dist'))
+app.use('/assets', express.static(root + '/node_modules/popper.js/dist/umd'))
+app.use('/assets', express.static(root + '/node_modules/bootstrap/dist'))
+app.use('/assets', express.static(root + '/node_modules/turbolinks/dist'))
+
+//
+// Routes
+//
+app.get("/", function(req, res) {
+  res.status(200).send("Hello, friend!")
+})
+
+if (!module.parent) {
+  app.listen(port, () => {
+    console.log(`Running express.js app on port ${port}`)
+  })
+}
+
+module.exports = app
---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const electron = require('electron')
+const desktop_app = electron.app
+const BrowserWindow = electron.BrowserWindow
+const path = require('path')
+
+let main_window = null
+
+desktop_app.on('ready', () => {
+  let title = "ThemeBuilder"
+  let port = 53011
+  let web_app = require(path.join(__dirname, '/config/application.js'))
+  let {width, height} = electron.screen.getPrimaryDisplay().workAreaSize
+  let main_window = new BrowserWindow({
+    width: width/2,
+    height: height,
+    center: true,
+    show: false,
+    title: title
+  })
+
+  web_app.listen(port, () => {
+    main_window.loadURL(`http://localhost:${port}`)
+    main_window.on('closed', () => {
+      desktop_app.quit()
+    })
+    main_window.on('ready-to-show', () => {
+      main_window.show()
+    })
+  })
+})

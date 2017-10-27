28d34b63caff4051a8c7fc82b1de651169513c23
Andy Bettisworth <andy@accreu.com>
2017-10-27 00:28:31 -0500

    convert to global __root

--- config/application.js
+++ config/application.js
@@ -12,16 +12,15 @@ const child_process = require('child_process')
  * Globals
  */
 
+global.__root = path.join(__dirname, '/..')
 let port = 53011
-let root = path.join(__dirname, '/..')
 
 /**
  * Locals
  */
 
 app.locals.title = "Theme Builder"
-app.locals.root  = root
-app.locals.views = root + '/app/views'
+app.locals.views = __root + '/app/views'
 
 /**
  * Settings
@@ -41,26 +40,26 @@ app.set('view engine', 'html.ejs')
  * Middleware
  */
 
-app.use(require(root + '/lib/middleware/setup_instructions'))
+app.use(require(__root + '/lib/middleware/setup_instructions'))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use(require(root + '/lib/middleware/serve_favicon')(app))
+app.use(require(__root + '/lib/middleware/serve_favicon'))
 
 /**
  * Static assets
  */
 
-app.use('/assets', express.static(root + '/node_modules/jquery/dist'))
-app.use('/assets', express.static(root + '/node_modules/popper.js/dist/umd'))
-app.use('/assets', express.static(root + '/node_modules/font-awesome'))
-app.use('/assets', express.static(root + '/tmp/bootstrap/dist'))
+app.use('/assets', express.static(__root + '/node_modules/jquery/dist'))
+app.use('/assets', express.static(__root + '/node_modules/popper.js/dist/umd'))
+app.use('/assets', express.static(__root + '/node_modules/font-awesome'))
+app.use('/assets', express.static(__root + '/tmp/bootstrap/dist'))
 
 /**
  * Routes
  */
 
 app.get("/", (req, res) => {
-  let scss = fs.readFileSync(root + '/tmp/bootstrap/scss/_custom.scss', 'utf8')
+  let scss = fs.readFileSync(__root + '/tmp/bootstrap/scss/_custom.scss', 'utf8')
   res.render("editor", { custom_scss: scss })
 })
 app.post("/update", (req, res, next) => {
@@ -71,15 +70,15 @@ app.post("/update", (req, res, next) => {
     } else {
       let custom_scss = fields.style.toLowerCase()
       if (custom_scss.length == 0) {
-        fs.writeFileSync(root + '/tmp/bootstrap/scss/_custom.scss', "// Drink the Sea" + os.EOL, 'utf8')
+        fs.writeFileSync(__root + '/tmp/bootstrap/scss/_custom.scss', "// Drink the Sea" + os.EOL, 'utf8')
       } else {
-        fs.writeFileSync(root + '/tmp/bootstrap/scss/_custom.scss', custom_scss + os.EOL, 'utf8')
+        fs.writeFileSync(__root + '/tmp/bootstrap/scss/_custom.scss', custom_scss + os.EOL, 'utf8')
       }
-      child_process.exec('npm install', { cwd: root + '/tmp/bootstrap' }, (err, stdout, stderr) => {
+      child_process.exec('npm install', { cwd: __root + '/tmp/bootstrap' }, (err, stdout, stderr) => {
         if (err) {
           next(err)
         } else {
-          child_process.exec('npm run css-main', { cwd: root + '/tmp/bootstrap' }, (err, stdout, stderr) => {
+          child_process.exec('npm run css-main', { cwd: __root + '/tmp/bootstrap' }, (err, stdout, stderr) => {
             if (err) {
               next(err)
             } else {
@@ -92,7 +91,7 @@ app.post("/update", (req, res, next) => {
   })
 })
 app.get("/download", (req, res) => {
-  let css_path = root + '/tmp/bootstrap/dist/css/bootstrap.min.css'
+  let css_path = __root + '/tmp/bootstrap/dist/css/bootstrap.min.css'
   res.download(css_path, 'bootstrap.min.css', (err) => {
     if (err) {
       next(err)
@@ -106,8 +105,8 @@ app.get("/download", (req, res) => {
  * Error handlers
  */
 
-app.use(require(root + '/lib/middleware/page_not_found'))
-app.use(require(root + '/lib/middleware/render_error'))
+app.use(require(__root + '/lib/middleware/page_not_found'))
+app.use(require(__root + '/lib/middleware/render_error'))
 
 
 /**

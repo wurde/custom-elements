c30bcc2276d476e7234e68ac54e5c5ff97458caa
Andy Bettisworth <andy@accreu.com>
2018-05-03 22:43:08 -0500

    double quotes

--- config/app.js
+++ config/app.js
@@ -1,16 +1,16 @@
-'use strict'
+"use strict"
 
 /**
  * Dependencies
  */
 
-const express = require('express')
-const path = require('path')
-const ejs = require('ejs')
-const fs = require('fs')
-const os = require('os')
-const formidable = require('formidable')
-const child_process = require('child_process')
+const express = require("express")
+const path = require("path")
+const ejs = require("ejs")
+const fs = require("fs")
+const os = require("os")
+const formidable = require("formidable")
+const child_process = require("child_process")
 
 /**
  * Initialize app
@@ -23,9 +23,9 @@ const app = express()
  */
 
 const port = 53011
-const base = path.join(__dirname, '..')
-const env = process.env.NODE_ENV || 'development'
-const views = path.join(base, '/app/views')
+const base = path.join(__dirname, "..")
+const env = process.env.NODE_ENV || "development"
+const views = path.join(base, "/app/views")
 const title = "Theme Builder"
 
 /**
@@ -41,41 +41,41 @@ app.locals.views = views
  * Settings
  */
 
-app.set('env', env)
-app.disable('x-powered-by')
+app.set("env", env)
+app.disable("x-powered-by")
 
 /**
  * View engine
  */
 
-app.engine('html.ejs', ejs.renderFile);
-app.set('view engine', 'html.ejs')
-app.set('views', views)
+app.engine("html.ejs", ejs.renderFile);
+app.set("view engine", "html.ejs")
+app.set("views", views)
 
 /**
  * Middleware
  */
 
-app.use(require(base + '/lib/middleware/setup_instructions'))
+app.use(require(base + "/lib/middleware/setup_instructions"))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use(require(base + '/lib/middleware/serve_favicon'))
+app.use(require(base + "/lib/middleware/serve_favicon"))
 
 /**
  * Static assets
  */
 
-app.use('/assets', express.static(base + '/node_modules/jquery/dist'))
-app.use('/assets', express.static(base + '/node_modules/popper.js/dist/umd'))
-app.use('/assets', express.static(base + '/node_modules/font-awesome'))
-app.use('/assets', express.static(base + '/tmp/bootstrap/dist'))
+app.use("/assets", express.static(base + "/node_modules/jquery/dist"))
+app.use("/assets", express.static(base + "/node_modules/popper.js/dist/umd"))
+app.use("/assets", express.static(base + "/node_modules/font-awesome"))
+app.use("/assets", express.static(base + "/tmp/bootstrap/dist"))
 
 /**
  * Routes
  */
 
 app.get("/", (req, res) => {
-  let scss = fs.readFileSync(base + '/tmp/bootstrap/scss/_custom.scss', 'utf8')
+  let scss = fs.readFileSync(base + "/tmp/bootstrap/scss/_custom.scss", "utf8")
   res.render("editor", { custom_scss: scss })
 })
 app.post("/update", (req, res, next) => {
@@ -86,15 +86,15 @@ app.post("/update", (req, res, next) => {
     } else {
       let custom_scss = fields.style.toLowerCase()
       if (custom_scss.length == 0) {
-        fs.writeFileSync(base + '/tmp/bootstrap/scss/_custom.scss', "// Drink the Sea" + os.EOL, 'utf8')
+        fs.writeFileSync(base + "/tmp/bootstrap/scss/_custom.scss", "// Drink the Sea" + os.EOL, "utf8")
       } else {
-        fs.writeFileSync(base + '/tmp/bootstrap/scss/_custom.scss', custom_scss + os.EOL, 'utf8')
+        fs.writeFileSync(base + "/tmp/bootstrap/scss/_custom.scss", custom_scss + os.EOL, "utf8")
       }
-      child_process.exec('npm install', { cwd: base + '/tmp/bootstrap' }, (err, stdout, stderr) => {
+      child_process.exec("npm install", { cwd: base + "/tmp/bootstrap" }, (err, stdout, stderr) => {
         if (err) {
           next(err)
         } else {
-          child_process.exec('npm run css-main', { cwd: base + '/tmp/bootstrap' }, (err, stdout, stderr) => {
+          child_process.exec("npm run css-main", { cwd: base + "/tmp/bootstrap" }, (err, stdout, stderr) => {
             if (err) {
               next(err)
             } else {
@@ -107,12 +107,12 @@ app.post("/update", (req, res, next) => {
   })
 })
 app.get("/download", (req, res, next) => {
-  let css_path = base + '/tmp/bootstrap/dist/css/bootstrap.min.css'
-  res.download(css_path, 'bootstrap.min.css', (err) => {
+  let css_path = base + "/tmp/bootstrap/dist/css/bootstrap.min.css"
+  res.download(css_path, "bootstrap.min.css", (err) => {
     if (err) {
       next(err)
     } else {
-      console.log('Sent:', 'bootstrap.min.css')
+      console.log("Sent:", "bootstrap.min.css")
     }
   })
 })
@@ -121,8 +121,8 @@ app.get("/download", (req, res, next) => {
  * Error handlers
  */
 
-app.use(require(base + '/lib/middleware/page_not_found'))
-app.use(require(base + '/lib/middleware/render_error'))
+app.use(require(base + "/lib/middleware/page_not_found"))
+app.use(require(base + "/lib/middleware/render_error"))
 
 
 /**

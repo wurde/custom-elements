fa47862d8df738d8ac46b0a27e4eb900c7754e37
Andy Bettisworth <andy@accreu.com>
2018-05-05 23:39:32 -0500

    ctrl: download theeme

--- app/controllers/root_controller.js
+++ app/controllers/root_controller.js
@@ -2,6 +2,14 @@
 
 module.exports = (app) => {
   /**
+   * Dependencies
+   */
+
+  const base = app.locals.base
+  const fs = require("fs")
+  const path = require("path")
+
+  /**
    * Define controller
    */
 
@@ -15,7 +23,8 @@ module.exports = (app) => {
      */
 
     static index(req, res) {
-      res.render("index")
+      let scss = fs.readFileSync(base + "/tmp/bootstrap/scss/_custom.scss", "utf8")
+      res.render("index", { custom_scss: scss })
     }
 
     /**
@@ -28,6 +37,60 @@ module.exports = (app) => {
 
     static redirect_to_index(req, res) {
       res.redirect("/")
+    }
+
+    /**
+     * Update theme.
+     *
+     * @static @method
+     * @since 1.0.4
+     * @public
+     */
+
+    static update(req, res) {
+      // var form = new formidable.IncomingForm()
+      // form.parse(req, (err, fields, files) => {
+      //   if (err) {
+      //     next(err)
+      //   } else {
+      //     let custom_scss = fields.style.toLowerCase()
+      //     if (custom_scss.length == 0) {
+      //       fs.writeFileSync(base + "/tmp/bootstrap/scss/_custom.scss", "// Drink the Sea" + os.EOL, "utf8")
+      //     } else {
+      //       fs.writeFileSync(base + "/tmp/bootstrap/scss/_custom.scss", custom_scss + os.EOL, "utf8")
+      //     }
+      //     child_process.exec("npm install", { cwd: base + "/tmp/bootstrap" }, (err, stdout, stderr) => {
+      //       if (err) {
+      //         next(err)
+      //       } else {
+      //         child_process.exec("npm run css-main", { cwd: base + "/tmp/bootstrap" }, (err, stdout, stderr) => {
+      //           if (err) {
+      //             next(err)
+      //           } else {
+      //             res.sendStatus(200)
+      //           }
+      //         })
+      //       }
+      //     })
+      //   }
+      // })
+      res.redirect("/")
+    }
+
+    /**
+     * Download theme.
+     *
+     * @static @method
+     * @since 1.0.4
+     * @public
+     */
+
+    static download(req, res) {
+      let css_path = path.join(base, "tmp", "bootstrap", "dist", "css", "bootstrap.min.css")
+
+      res.download(css_path, "bootstrap.min.css", (err) => {
+        if (err) { next(err) }
+      })
     }
   }
 
--- config/routes/root_router.js
+++ config/routes/root_router.js
@@ -19,54 +19,18 @@ module.exports = (app) => {
    * Routes
    */
 
+  // GET /
   router.route("/")
     .get(RootController.index)
-  // app.get("/", (req, res) => {
-  //   let scss = fs.readFileSync(base + "/tmp/bootstrap/scss/_custom.scss", "utf8")
-  //   res.render("editor", { custom_scss: scss })
-  // })
 
-  router.route("/")
+  // GET,POST /update
+  router.route("/update")
     .get(RootController.redirect_to_index)
-    .post(RootController.index)
-  // app.post("/update", (req, res, next) => {
-  //   var form = new formidable.IncomingForm()
-  //   form.parse(req, (err, fields, files) => {
-  //     if (err) {
-  //       next(err)
-  //     } else {
-  //       let custom_scss = fields.style.toLowerCase()
-  //       if (custom_scss.length == 0) {
-  //         fs.writeFileSync(base + "/tmp/bootstrap/scss/_custom.scss", "// Drink the Sea" + os.EOL, "utf8")
-  //       } else {
-  //         fs.writeFileSync(base + "/tmp/bootstrap/scss/_custom.scss", custom_scss + os.EOL, "utf8")
-  //       }
-  //       child_process.exec("npm install", { cwd: base + "/tmp/bootstrap" }, (err, stdout, stderr) => {
-  //         if (err) {
-  //           next(err)
-  //         } else {
-  //           child_process.exec("npm run css-main", { cwd: base + "/tmp/bootstrap" }, (err, stdout, stderr) => {
-  //             if (err) {
-  //               next(err)
-  //             } else {
-  //               res.sendStatus(200)
-  //             }
-  //           })
-  //         }
-  //       })
-  //     }
-  //   })
-  // })
-  // app.get("/download", (req, res, next) => {
-  //   let css_path = base + "/tmp/bootstrap/dist/css/bootstrap.min.css"
-  //   res.download(css_path, "bootstrap.min.css", (err) => {
-  //     if (err) {
-  //       next(err)
-  //     } else {
-  //       console.log("Sent:", "bootstrap.min.css")
-  //     }
-  //   })
-  // })
+    .post(RootController.update)
+
+  // GET /download
+  router.route("/download")
+    .get(RootController.download)
 
   /**
    * Export router.

6ac8ba77db683f10fa2bbb17da757b4ace84621a
Andy Bettisworth <andy@accreu.com>
2019-09-20 03:32:15 -0500

    Launch electron desktop app

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log('Pending major rewrite.')
+'use strict'
+
+/**
+ * Dependencies
+ */
+
+const electron = require('electron')
+
+/**
+ * Constants
+ */
+
+const electron_app = electron.app
+const BrowserWindow = electron.BrowserWindow
+const title = "Theme Builder"
+
+/**
+ * Protect main_window from garbage collection.
+ */
+
+let main_window = null
+
+/**
+ * Define function to create window.
+ */
+
+function create_window() {
+  const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize
+
+  let main_window = new BrowserWindow({
+    title: title,
+    width: width - (width / 4),
+    height: height,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  main_window.loadURL(`file://${__dirname}/../client/dist/index.html`)
+}
+
+/**
+ * Mount handler for Electron 'ready' event.
+ */
+
+electron_app.on("ready", create_window)

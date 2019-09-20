89171278053cc8d4a1671d4d3876f07c870b047c
Andy Bettisworth <andy@accreu.com>
2019-09-20 03:35:42 -0500

    Clean react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,25 @@
+/**
+ * Dependencies
+ */
+
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+/**
+ * Define component
+ */
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        Theme Builder
       </header>
     </div>
   );
 }
+
+/**
+ * Export component
+ */
 
 export default App;

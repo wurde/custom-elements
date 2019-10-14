2c304418c70dfc481be6da896e9c63f9c37b8633
Andy Bettisworth <andy@accreu.com>
2019-10-14 02:31:09 -0500

    Connect Electron and React

--- index.js
+++ index.js
@@ -4,50 +4,48 @@
  * Dependencies
  */
 
-const url = require('url')
-const path = require('path')
-const electron = require('electron')
+const electron = require('electron');
 
 /**
  * Constants
  */
 
-const electron_app = electron.app
-const BrowserWindow = electron.BrowserWindow
-const title = "Theme Builder"
+const electron_app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const port = process.env.PORT || 3000;
+const title = "Theme Builder";
 
 /**
  * Protect main_window from garbage collection.
  */
 
-let main_window = null
+let main_window = null;
 
 /**
  * Define function to create window.
  */
 
 function create_window() {
-  const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize
+  const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize;
 
   let main_window = new BrowserWindow({
     title: title,
     width: width - (width / 4),
     height: height,
+    show: false,
+    center: true,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      sandbox: false
     }
-  })
+  });
 
-  const index_path = url.format({
-    pathname: path.join(__dirname, 'client/build/index.html'),
-    protocol: 'file'
-  })
-
-  main_window.loadURL(index_path)
+  main_window.loadURL(`http://localhost:${port}`);
+  main_window.once('ready-to-show', main_window.show);
 }
 
 /**
  * Mount handler for Electron 'ready' event.
  */
 
-electron_app.on("ready", create_window)
+electron_app.on("ready", create_window);
